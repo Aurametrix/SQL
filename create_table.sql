c04c15bca9eeb2d26532676eb03e984f351ad034
Irene <irene@aurametrix.com>
2014-08-11 20:51:47 -0700

    creating a table

---
+++ create_table.sql
@@ -0,0 +1,11 @@
+/*To create a database
+CREATE DATABASE library;
+*/
+
+/*To create a table 
+specify a name for the table column
+and its data type */
+
+CREATE TABLE myLibrary (all_my_nooks VARCHAR(4000));
+
+			
