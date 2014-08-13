ac79f4c854040235bcdb3dfd74f4d105f5382d71
Irene <irene@aurametrix.com>
2014-08-12 20:26:38 -0700

    varchar data type

--- create_table.sql
+++ create_table.sql
@@ -8,4 +8,16 @@ and its data type */
 
 CREATE TABLE myLibrary (all_my_nooks VARCHAR(4000));
 
-			
+---- The column all_my_books is defined as a character data type
+---- It can hold as many as 4,000 characters
+
+/* another example
+CREATE TABLE Persons
+(
+PersonID int,
+LastName varchar(255),
+FirstName varchar(255),
+Address varchar(255),
+City varchar(255)
+);
+ */
