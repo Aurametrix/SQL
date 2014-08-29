ffd73243090bb3088d9a3b725b61c312e56afa39
Irene <irene@aurametrix.com>
2014-08-28 22:03:58 -0700

    Discovering SQL

---
+++ DiscoveringSQL/insert.sql
@@ -0,0 +1,17 @@
+USE Library
+INSERT INTO myLibrary VALUES ('SQL Bible by Alex Kriegel Boris M. Trukhov
+Paperback: 888 pages Publisher: Willeyl 2 edition (April 7, 2008)
+Language: English ISBN-13: 978-9470229064');
+
+INSERT INTO myLibrary VALUES ('Microsoft SQL Server 2000 Weekend Crash Course by 
+Alex Kriegel Paperback: 408 pages Publisher: Wiley (October 15, 2001) 
+Language:English ISBN-13: 978-0764548406’
+);
+INSERT INTO myLibrary (all_my_books ) VALUES ('Letters From The Earth by Mark Twain
+Paperback: 52 pages Publisher: Greenbook Publications, LLC (June 7, 2010) 
+Language:English ISBN-13: 978-1617430060’);
+
+INSERT INTO myLibrary (all_my_books ) VALUES ('Mindswap by Robert Sheckley 
+Paperback: 224 pages Publisher: Orb Books (May 30, 2006) 
+Language:English ISBN-13: 978-0765315601’);
+
--- insert.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO myLibrary VALUES ('SQL Bible by Alex Kriegel Boris M. Trukhov
-Paperback: 888 pages Publisher: Willeyl 2 edition (April 7, 2008)
-Language: English ISBN-13: 978-9470229064');
