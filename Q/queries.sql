c03952a7042a1bc9e91d2a97b6e4ead3b5b65b9c
Aurametrix <irene@aurametrix.com>
2016-03-27 20:04:15 -0700

    simplest

---
+++ Q/queries.sql
@@ -0,0 +1,9 @@
+-- Get unique DEPARTMENT from employee table
+select distinct DEPARTMENT from EMPLOYEE
+
+
+-- Select first 3 characters of FIRST_NAME from EMPLOYEE
+
+-- Oracle Equivalent of SQL Server SUBSTRING is SUBSTR, Query : select substr(FIRST_NAME,0,3) from employee
+
+SQL Server Equivalent of Oracle SUBSTR is SUBSTRING, Query : select substring(FIRST_NAME,0,3) from employee
