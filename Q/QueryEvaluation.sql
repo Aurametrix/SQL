067e042927e1d2e79039195c2efd727b1236cb40
Aurametrix <irene@aurametrix.com>
2016-04-09 12:13:24 -0700

    Postgres’ version of Selinger Optimizer

--- Q/QueryEvaluation.sql
+++ Q/QueryEvaluation.sql
@@ -21,3 +21,27 @@ CREATE TABLE Nominees (
 SELECT m.title
 FROM Movies m, Nominees n
 WHERE m.mid = n.mid AND n.year = 2016;
+
+''
+
+// optimizer
+
+
+// left-deep and right-deep
+for each set of (i-1) relations S begin
+    for each relation R not in S begin
+        consider all possible algorithms to join bestjoin[S] with R
+        consider all possible algorithms to join R with bestjoin[S]
+        pick the best one, and set bestjoin[S + R] to that plan
+    end
+end
+
+// bushy
+for j = 2 to (i-2) begin
+    for each set of j relations S1 begin
+        for each set of (i-j) relations S2 begin
+            consider all plans to join bestjoin[S1] with bestjoin[S2]
+            set bestjoin[S1+S2] to the best plan if better than current
+        end
+    end
+end
