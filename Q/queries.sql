acfc75f3bc2cb062773a85a385212acd5e0927fe
Aurametrix <irene@aurametrix.com>
2016-05-02 21:07:56 -0700

    emove duplicates within select query

--- Q/queries.sql
+++ Q/queries.sql
@@ -7,3 +7,6 @@ select distinct DEPARTMENT from EMPLOYEE
 -- Oracle Equivalent of SQL Server SUBSTRING is SUBSTR, Query : select substr(FIRST_NAME,0,3) from employee
 
 SQL Server Equivalent of Oracle SUBSTR is SUBSTRING, Query : select substring(FIRST_NAME,0,3) from employee
+
+-- remove duplicates
+SELECT DISTINCT CAST(start_date AS DATE) FROM table;
